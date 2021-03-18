ac4386e9b49b1496fbb6186b4f451a6ec397fd8c
Orion Junkins <junkinso@oregonstate.edu>
2021-03-18 12:02:32 -0700

    WaterDispenser: spec refactored

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,24 +1,21 @@
 require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
+
+  let(:reservoir) {double()}
+  let(:dispenser) {WaterDispenser.new(reservoir)}
+  let(:vessel) {Vessel.new}
   
   it 'can be drained' do
-    reservoir = double()
     expect(reservoir).to receive(:drain)
-
     vessel = double()
     allow(vessel).to receive(:volume).and_return(nil)
     allow(vessel).to receive(:fill).and_return(nil)
-
-    dispenser = WaterDispenser.new(reservoir)
     dispenser.dispense(vessel)
   end
 
   it 'fills the given vessel when drained' do
-    vessel = Vessel.new
-    reservoir = double()
     allow(reservoir).to receive(:drain).and_return(vessel.volume)
-    dispenser = WaterDispenser.new(reservoir)
     dispenser.dispense(vessel)
     expect(vessel).to_not be_empty
   end
