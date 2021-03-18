5941830bdcd80f66ffa22617208c3dae02bc92b1
Orion Junkins <junkinso@oregonstate.edu>
2021-03-18 11:22:04 -0700

    WaterDispenser: basic functionality under test

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,5 +1,16 @@
 require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
+  
+  it 'can be drained' do
+    reservoir = double()
+    expect(reservoir).to receive(:drain)
+
+    vessel = double()
+    allow(vessel).to receive(:volume).and_return(nil)
+
+    dispenser = WaterDispenser.new(reservoir)
+    dispenser.dispense(vessel)
+  end
 
 end
