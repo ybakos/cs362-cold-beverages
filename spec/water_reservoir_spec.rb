da28a745715e4de40997522bd78ae6b65c3de5b9
Brayden Brown <braydenbrown@gmail.com>
2021-03-18 11:45:54 -0700

    Vessel: change to empty method to make it more pragmatic

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,5 +1,12 @@
 require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
+  before do
+    @res = WaterReservoir.new(10, 20)
+  end
 
+  it 'has a capacity and initial_water_volume' do
+    expect(@res.capacity).to eq(10)
+    expect(@res.initial_water_volume).to eq(20)
+  end
 end
