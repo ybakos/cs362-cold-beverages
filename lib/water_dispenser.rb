216a47908526ebd1ccbb8ca0120256c96efa4ce5
Sergio Urrutia-Oyer <urrutise@oregonstate.edu>
2021-03-18 12:08:37 -0700

    realized I inherently added the draining and filling functionalities when I was doing the water_dispenser test...but I did just now fix the negative volume

--- lib/water_dispenser.rb
+++ lib/water_dispenser.rb
@@ -7,7 +7,17 @@ class WaterDispenser
   end
 
   def dispense(vessel)
-    reservoir.drain(vessel.volume)
+    if check_reservoir_volume() == 1
+      reservoir.drain(vessel.volume)
+    end
   end
 
+  def check_reservoir_volume()
+    if @reservoir.empty?
+      puts 'reservoir is empty, nothing dispensed'
+    else
+      return 1
+    end
+
+  end
 end
