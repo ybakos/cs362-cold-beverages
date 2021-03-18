5beffd702a0bd9ce8eb7639c5c8d2675f01f7792
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-18 11:39:47 -0700

    Add instantiation and attribute tests

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -2,4 +2,18 @@ require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
 
+  let(:reservoir) { WaterReservoir.new(10, 0) }
+
+	it 'exists' do
+		reservoir
+	end
+
+	it 'has a capacity' do
+		expect(reservoir.capacity).to eq(10)
+	end
+
+	it 'has a current_water_volume' do
+		expect(reservoir.current_water_volume).to eq(0)
+	end
+
 end
