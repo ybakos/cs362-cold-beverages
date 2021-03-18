21344159a9819eccf3d67a1d8c886e0447b890ac
Orion Junkins <junkinso@oregonstate.edu>
2021-03-18 11:24:26 -0700

    WaterReservoir: attribute characterization test

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,5 +1,13 @@
 require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
+  let(:reservoir) {WaterReservoir.new}
 
+  it 'has a capacity' do
+    expect(reservoir).to respond_to(:capacity)
+  end
+
+  it 'has a current water volume' do
+    expect(reservoir).to respond_to(:current_water_volume)
+  end
 end
