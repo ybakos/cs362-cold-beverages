ba7bbd7cfae34b307bc8862c5b8abe54f351f4a7
Adrianna Gilmore <a_inman33@yahoo.com>
2021-03-18 11:42:34 -0700

    added attribute test for name in item_spec.rb

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
+    item = Item.new("Fake", 100)
+
+    it 'has a name' do
+      expect(item).to respond_to(:name)
+    end
 
 end
