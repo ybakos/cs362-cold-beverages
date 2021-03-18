87f1b71d11f8b0e65d64b5599efaa74d46de2e06
Aidan-B1409 <aidanb1049@gmail.com>
2021-03-18 11:40:10 -0700

    characterization tests for water reservior

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,5 +1,16 @@
 require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
+  capacity = 15
+  initial_water_volume = 0
+  let(:reservoir) { WaterReservoir.new(capacity, initial_water_volume) }
+
+  it 'has a capacity' do
+    expect(reservoir.capacity).to eq(capacity)
+  end
+
+  it 'has an initial volume' do
+    expect(reservoir.current_water_volume).to eq(initial_water_volume)
+  end
 
 end
