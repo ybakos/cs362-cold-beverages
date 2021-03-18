db9e4f357db2c669e1fc6371622e1a5b6a8ef3ce
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-18 11:40:05 -0700

    Add instantiation and attribute tests

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,5 +1,16 @@
 require_relative '../lib/water_dispenser'
+require_relative '../lib/vessel'
 
 describe 'A water dispenser' do
+
+	let(:dispenser) { WaterDispenser.new('FAKE') }
+
+  it 'exists' do
+		dispenser
+	end
+
+	it 'has a reservoir' do
+		expect(dispenser.reservoir).to eq('FAKE')
+	end
 
 end
