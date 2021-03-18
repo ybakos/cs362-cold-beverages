4b60b1afd531396c90db13af062274c37ae60cba
Aidan-B1409 <aidanb1049@gmail.com>
2021-03-18 11:48:10 -0700

    Characterization and power tests for freezer

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -1,5 +1,19 @@
 require_relative '../lib/freezer'
 
 describe 'A freezer' do
+  let(:freezer) { Freezer.new }
 
+  it 'has a default capacity of 100' do
+    expect(freezer.capacity).to eq(100)
+  end
+
+  it 'can turn on the power' do
+    freezer.turn_on
+    expect(freezer.power).to eq(:on)
+  end
+
+  it 'can turn off the power' do
+    freezer.turn_off
+    expect(freezer.power).to eq(:off)
+  end
 end
