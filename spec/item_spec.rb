0e393169edb85361098f633f98c82ec1431a8091
Danielle Puga <danielle.puga@gmail.com>
2021-03-18 12:10:30 -0700

    Item under test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,11 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
+  let(:item) do Item.new
+  end
 
+  it 'has a name and volume' do
+    expect(item).to respond_to(:name)
+    expect(item).to respond_to(:volume)
+  end
 end
