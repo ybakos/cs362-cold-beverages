b7d3db7aaad36fe1250df35f138c7d8658510a59
Adrianna Gilmore <a_inman33@yahoo.com>
2021-03-18 11:46:43 -0700

    added instantiation test to item_spec.rb

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,6 +3,10 @@ require_relative '../lib/item'
 describe 'An item of food or a beverage' do
   item = Item.new("Fake", 100)
 
+  it 'exists' do
+    Item.new("Fake", 100)
+  end
+
   it 'has a name' do
     expect(item).to respond_to(:name)
   end
