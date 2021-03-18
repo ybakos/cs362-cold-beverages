c820e4c7eed485b0e8eed12b567988db1e25a041
Brayden Brown <braydenbrown@gmail.com>
2021-03-18 11:29:24 -0700

    Vessel: empty? and fill mewthods created to pass the tests.

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,20 @@
 class Vessel
-  attr_reader :volume
+  attr_reader :volume, :name
 
   def initialize(name, volume)
     @name = name
     @volume = volume
+  end
+
+  def empty?
+    if @volume == 0
+      TRUE
+    else
+      FALSE
+    end
+  end
+
+  def fill
   end
 
 end
