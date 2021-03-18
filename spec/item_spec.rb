b2c1d6ab91e3b072a353826558a7fd7263ab7165
Aidan-B1409 <aidanb1049@gmail.com>
2021-03-18 11:20:37 -0700

    Characterization tests for item

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,15 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
+  name = 'FAKE'
+  volume = 100
+  let(:item) { Item.new(name, volume) }
+  it 'has a name' do
+    expect(item.name).to eq(name)
+  end
+
+  it 'has a volume' do
+    expect(item.volume).to eq(volume)
+  end
 
 end
