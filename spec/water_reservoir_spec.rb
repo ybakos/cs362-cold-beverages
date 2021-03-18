74d1abf783897609590d1eb9861624ff1aff7ed1
Adrianna Gilmore <a_inman33@yahoo.com>
2021-03-18 12:03:51 -0700

    added attribute test for capacity to water_resivoir_spec

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,7 +1,13 @@
 require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
+  water_reservoir = WaterReservoir.new
+
   it 'exists' do
     WaterReservoir.new
+  end
+
+  it 'has a capacity' do
+    expect(water_reservoir).to respond_to(:capacity)
   end
 end
