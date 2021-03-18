d69981c5bf56bacbe1c336db2053142296efb772
Erin O'Connell <oconerin@oregonstate.edu>
2021-03-18 12:09:09 -0700

    tested negative drain issue in water_res

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -2,4 +2,9 @@ require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
 
+    it 'cannot drain passed a 0 volume' do 
+        water_reservoir = WaterReservoir.new(10,10)
+        water_reservoir.drain(13)
+        expect(water_reservoir.current_water_volume).to eq(10)
+    end    
 end
