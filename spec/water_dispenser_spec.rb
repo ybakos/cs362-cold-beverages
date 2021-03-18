bb08846878141e9e7241ef21f96f70fd0e90d32e
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-18 12:19:18 -0700

    Attempt to add tests for dispenser

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/water_dispenser'
+require_relative '../lib/vessel'
 
 describe 'A water dispenser' do
 
@@ -12,15 +13,26 @@ describe 'A water dispenser' do
 		expect(dispenser.reservoir).to eq('FAKE')
 	end
 
-	# describe '#dispense' do
-	# 	it 'decreases the reservoir volume' do
-	# 		vessel = double()
-	# 		allow(vessel).to receive(:volume) { 10 }
-	# 		reservoir = double()
-	# 		allow(reservoir).to receive(:drain)
-	# 		expect(reservoir.drain(vessel)).to eq(reservoir.volume-vessel.volume)
+	describe '#dispense' do
+		let(:vessel) { Vessel.new(volume=10) }
 
-	# 	end
-	# end
+		# it 'decreases the reservoir volume' do
+		# 	# vessel = double()
+		# 	# allow(vessel).to receive(:volume) { 10 }
+		# 	reservoir = double()
+		# 	allow(reservoir).to receive(:current_water_volume)
+		# 	allow(reservoir).to receive(:drain)
+		# 	remaining_volume = reservoir.current_water_volume
+		# 	expect(reservoir.drain(vessel)).to be < res
+		# end
+
+		# it 'fills the vessel' do
+		# 	dispenser = double()
+		# 	allow(dispenser).to receive(:reservoir) {}
+		# 	allow(dispenser).to receive(:drain)
+		# 	dispenser.drain(vessel)
+		# 	expect(vessel).to_not be_empty
+		# end
+	end
 
 end
