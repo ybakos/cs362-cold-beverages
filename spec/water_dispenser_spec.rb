4cc9be60ce12858c04718656f3944b11ba9dd1b1
Erin O'Connell <oconerin@oregonstate.edu>
2021-03-18 11:40:09 -0700

    passed has-a-reservoir for water_despenser

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -6,4 +6,9 @@ describe 'A water dispenser' do
         #exists
     end  
 
+    it 'has a reservoir' do
+         dispenser= WaterDispenser.new(5)
+        expect(dispenser.reservoir).to eq(5)
+    end
+    
 end
