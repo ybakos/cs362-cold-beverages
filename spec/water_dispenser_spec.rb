9ba3a276f0657fd394137e0177d7dd6da4f4488a
Danielle Puga <danielle.puga@gmail.com>
2021-03-18 12:12:46 -0700

    Water Reservoir started

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,5 +1,12 @@
 require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
+  let(:water_dispenser) do WaterDispenser.new
+  end
 
-end
+  it 'has a reservoir' do
+    expect(water_dispenser).to respond_to(:reservoir)
+  end
+
+  it 'drains from the vessel' do
+  end
