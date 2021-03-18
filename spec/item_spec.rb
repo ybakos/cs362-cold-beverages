592ee785756ce40e9dca352e4216e04a27e563d7
Brayden Brown <braydenbrown@gmail.com>
2021-03-18 11:32:45 -0700

    Item: test initialization with a volume and name

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,13 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
+  before do
+    @item = Item.new('FAKE', 200)
+  end
+
+  it 'has a name and volume' do
+    expect(@item.name).to eq('FAKE')
+    expect(@item.volume).to eq(200)
+  end
 
 end
