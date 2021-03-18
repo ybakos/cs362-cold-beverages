030383e12fd400f032c253a955b3a8b5abd731fa
Aidan-B1409 <aidanb1049@gmail.com>
2021-03-18 11:51:55 -0700

    More freezer tests

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -16,4 +16,20 @@ describe 'A freezer' do
     freezer.turn_off
     expect(freezer.power).to eq(:off)
   end
+
+  it 'can add an item to the freezer' do
+    item = double
+    allow(item).to receive('volume').and_return(5)
+    freezer.add(item)
+    expect(freezer.remaining_capacity).to_not eq(freezer.capacity)
+  end
+
+  it 'knows the remaining capacity of the freezer' do
+    expect(freezer.remaining_capacity).to eq(freezer.capacity)
+  end
+
+  it 'can set the temperature of the freezer' do
+    freezer.set_level(5)
+    expect(freezer.temperature).to eq(70 - 5 * 10)
+  end
 end
