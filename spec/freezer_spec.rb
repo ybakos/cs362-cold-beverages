53a0e418919395732db0e26908ab52cf1192337f
Orion Junkins <junkinso@oregonstate.edu>
2021-03-18 11:39:27 -0700

    Freezer: more complex characterization tests

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -2,6 +2,7 @@ require_relative '../lib/freezer'
 
 describe 'A freezer' do
   let(:freezer) {Freezer.new}
+  before {ROOM_TEMPERATURE = 70}
   
   it 'has a capacity' do
     expect(freezer).to respond_to(:capacity)
@@ -24,6 +25,22 @@ describe 'A freezer' do
     freezer.turn_on
     freezer.turn_off
     expect(freezer.power).to eq(:off)
+  end
+
+  it 'has a reduced capacity after adding an item' do
+    item_volume = 1
+    item = double()
+    allow(item).to receive(:volume).and_return(item_volume)
+    initial_capacity = freezer.capacity
+    freezer.add(item)
+    expect(freezer.remaining_capacity).to eq(initial_capacity - item_volume)
+  end
+
+  it 'can have its level set' do
+    level = 30
+    expected_temp = ROOM_TEMPERATURE - level * 10
+    freezer.set_level(level)
+    expect(freezer.temperature).to eq(expected_temp)
   end
 
 end
