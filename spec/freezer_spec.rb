39acf34aa9efcbcda1d846e19fe057156f207218
Orion Junkins <junkinso@oregonstate.edu>
2021-03-18 11:31:09 -0700

    Freezer: basic member var characterization tests

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -1,5 +1,15 @@
 require_relative '../lib/freezer'
 
 describe 'A freezer' do
+  let(:freezer) {Freezer.new}
+  
+  it 'has a capacity' do
+    expect(freezer).to respond_to(:capacity)
+  end
 
+  it 'has a temperature' do
+    expect(freezer).to respond_to(:temperature)
+  end
+
+  
 end
