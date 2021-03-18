01ece43b139c6885cf206585b97766611791cab1
Sergio Urrutia-Oyer <urrutise@oregonstate.edu>
2021-03-18 11:30:33 -0700

    item has name

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,7 +1,12 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
+  let(:item) { Item.new("FAKE",100) }
   it 'item exists' do
-    Item.new("FAKE","FAKE")
+    item
+  end
+
+  it 'item has name' do
+    expect(item).to respond_to(:name)
   end
 end
