cd0f4868ff95fba25c442fa12c17de3cfc35c67b
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-03-18 11:47:58 -0700

    Add emptiness tests

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -16,4 +16,24 @@ describe 'A water reservoir' do
 		expect(reservoir.current_water_volume).to eq(0)
 	end
 
+	describe 'emptiness' do
+		before do
+			reservoir = WaterReservoir.new(20, 0)
+		end
+
+		it 'is initially empty' do
+			expect(reservoir).to be_empty
+		end
+
+		it 'has a current_water_volume equal to capacity' do
+			reservoir.fill
+			expect(reservoir.current_water_volume).to eq(reservoir.capacity)
+		end
+
+		it 'is no longer empty when we fill it' do
+			reservoir.fill
+			expect(reservoir).to_not be_empty
+		end
+	end
+
 end
