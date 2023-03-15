4d66eea919043c906739351a106f2e6bb4f06b15
Julian Canepa <julian.l.canepa@gmail.com>
2023-03-15 16:20:18 -0700

    Create new fridge object in spec with all its merry band of dependencies.

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -1,6 +1,17 @@
 require_relative 'spec_helper'
 require_relative '../lib/refrigerator'
+require_relative '../lib/chiller'
+require_relative '../lib/freezer'
+require_relative '../lib/water_dispenser'
+require_relative '../lib/water_reservoir'
 
 describe 'A refrigerator' do
+  let(:refrigerator) {
+    Refrigerator.new(
+      Chiller.new,
+      Freezer.new,
+      WaterReservoir.new,
+      WaterDispenser.new
+    )}
 
 end
