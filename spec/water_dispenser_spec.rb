c071e3043b187d7b4ed78db80062be19f4be85c0
Julian Canepa <julian.l.canepa@gmail.com>
2023-03-15 14:47:10 -0700

    Implement water dispenser specs, without creating any dependency objects. Does not cover the "happy path"

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -3,4 +3,17 @@ require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
 
+  it 'can be instantiated' do
+    WaterDispenser.new(nil)
+  end
+
+  it 'can dispense' do
+    w = WaterDispenser.new(nil)
+    expect(w).to respond_to('dispense')
+  end
+
+  it 'cannot dispense without a resevoir or vessel' do
+    w = WaterDispenser.new(nil)
+    expect(w.dispense(nil)).to raise_error
+  end
 end
