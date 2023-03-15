33a0e4f561c01a74fb880a256c873167eb0e4b0a
Julian Canepa <julian.l.canepa@gmail.com>
2023-03-15 15:09:49 -0700

    Test instantiation for water reservoir.

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -2,5 +2,13 @@ require_relative 'spec_helper'
 require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
+  let(:reservoir) { WaterReservoir.new }
 
+  it 'can be instantiated' do
+    reservoir
+  end
+
+  it 'has an initial state' do
+
+  end
 end
