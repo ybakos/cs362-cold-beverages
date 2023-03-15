1cfd374c91f3d33f2e12b40d02866d438f3dea01
Julian Canepa <julian.l.canepa@gmail.com>
2023-03-15 15:33:44 -0700

    Verify freezer has a default capacity, refactor creation method in spec file.

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -3,7 +3,13 @@ require_relative '../lib/freezer'
 
 describe 'A freezer' do
 
+  let(:freezer) { Freezer.new }
+
   it 'can be instantiated' do
-    Freezer.new
+    freezer
+  end
+
+  it 'has a default initial capacity' do
+    expect(freezer.capacity).to eq(100)
   end
 end
