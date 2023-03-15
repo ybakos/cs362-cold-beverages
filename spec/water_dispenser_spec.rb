80894d2729e14afca98f349955c4a5d2da17a23f
Julian Canepa <julian.l.canepa@gmail.com>
2023-03-15 14:59:56 -0700

    Refactor water dispenser spec to use creational pattern.

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -3,6 +3,9 @@ require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
 
+  let(:resevoir) { double('Reservoir') }
+  let(:vessel) { double('Vessel') }
+
   it 'can be instantiated' do
     WaterDispenser.new(nil)
   end
@@ -12,8 +15,11 @@ describe 'A water dispenser' do
     expect(w).to respond_to('dispense')
   end
 
-  it 'cannot dispense without a resevoir or vessel' do
-    w = WaterDispenser.new(nil)
-    expect(w.dispense(nil)).to raise_error
+  it 'can dispense given a resevoir and vessel' do
+    allow(resevoir).to receive(:drain).and_return(nil)
+    allow(vessel).to receive(:volume).and_return(nil)
+
+    value = WaterDispenser.new(resevoir).dispense(vessel)
+    expect(value).to eq(nil)
   end
 end
