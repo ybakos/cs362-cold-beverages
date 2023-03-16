ed2a629aa542332cc9c0ce0b064366368fe4e4dd
Seth Weiss <sweisss@gmail.com>
2023-03-15 20:37:04 -0700

    Use let in water_dispenser_spec

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -3,15 +3,14 @@ require_relative '../lib/water_reservoir'
 require_relative '../lib/vessel.rb'
 
 describe 'A water dispenser' do
+  let(:water_reservoir) { WaterReservoir.new(100, 100) }
+  let(:water_dispenser) { WaterDispenser.new(water_reservoir) }
 
   it 'has a reservoir' do
-    water_dispenser = WaterDispenser.new(100)
-    expect(water_dispenser.reservoir).to eq(100)
+    expect(water_dispenser.reservoir).to be(water_reservoir)
   end
 
   it 'can dispense to a vessel' do
-    water_reservoir = WaterReservoir.new(100, 100)
-    water_dispenser = WaterDispenser.new(water_reservoir)
     vessel = Vessel.new(100)
     water_dispenser.dispense(vessel)
     expect(water_reservoir.current_water_volume).to eq(0)
