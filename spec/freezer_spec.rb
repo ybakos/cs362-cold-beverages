0c271bb37321b2cb967fe2e65127b41a2a836c17
Seth Weiss <sweisss@gmail.com>
2023-03-15 20:32:26 -0700

    Remove old code

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -4,13 +4,11 @@ describe 'A freezer' do
   let(:freezer) { Freezer.new }
 
   it 'has a capacity and temperature' do
-    # freezer = Freezer.new
     expect(freezer.capacity).to eq(100)
     expect(freezer.temperature).to eq(70)
   end
 
   it 'can turn its power on' do
-    # freezer = Freezer.new
     freezer.turn_on
     expect(freezer.power).to eq(:on)
   end
