fbe773c5deccacdf234531a427df8b00f0b6cd8a
Seth Weiss <sweisss@gmail.com>
2023-03-15 20:51:05 -0700

    Add initialization test in chiller_spec

--- spec/chiller_spec.rb
+++ spec/chiller_spec.rb
@@ -1,5 +1,11 @@
 require_relative '../lib/chiller'
 
 describe 'A chiller' do
+  let(:chiller) { Chiller.new }
+
+  it 'has a capacity and temperature' do
+    expect(chiller.capacity).to eq(100)
+    expect(chiller.temperature).to eq(70)
+  end
 
 end
