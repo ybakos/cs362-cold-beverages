caf269122c8f3404964a9e8fbe5b1e2fa63759ae
Seth Weiss <sweisss@gmail.com>
2023-03-15 20:32:16 -0700

    Move freezer to let

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -1,15 +1,16 @@
 require_relative '../lib/freezer'
 
 describe 'A freezer' do
+  let(:freezer) { Freezer.new }
 
   it 'has a capacity and temperature' do
-    freezer = Freezer.new
+    # freezer = Freezer.new
     expect(freezer.capacity).to eq(100)
     expect(freezer.temperature).to eq(70)
   end
 
   it 'can turn its power on' do
-    freezer = Freezer.new
+    # freezer = Freezer.new
     freezer.turn_on
     expect(freezer.power).to eq(:on)
   end
