59cb26995dfb3b9e74f0b54df568052b9783a56e
Seth Weiss <sweisss@gmail.com>
2023-03-15 20:45:35 -0700

    Update add items test in freezer_spec

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -2,6 +2,8 @@ require_relative '../lib/freezer'
 
 describe 'A freezer' do
   let(:freezer) { Freezer.new }
+  let(:dumplings) { Item.new('dumplings', 50) }
+  let(:beyond_brats) { Item.new('Beyond Brats', 4) }
 
   it 'has a capacity and temperature' do
     expect(freezer.capacity).to eq(100)
@@ -19,9 +21,15 @@ describe 'A freezer' do
   end
 
   it 'can add items' do
+    freezer.add(dumplings)
+    freezer.add(beyond_brats)
+    expect(freezer.contents).to eq([dumplings, beyond_brats])
+  end
+
+  it 'can calculate its remaining capacity' do skip
     freezer.add('dumplings')
     freezer.add('Beyond Brats')
-    expect(freezer.contents).to eq(['dumplings', 'Beyond Brats'])
+    expect(freezer.remaining_capacity).to eq(98)
   end
-  
+
 end
