e9e99d5152281b3839f44ab1994d5b9cef6514f7
Seth Weiss <sweisss@gmail.com>
2023-03-15 19:22:30 -0700

    Add initialization test for water_reservoir_spec

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -2,4 +2,10 @@ require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
 
+  it 'has an initial capacity and water volume' do
+    water_reservoir = WaterReservoir.new
+    expect(water_reservoir.capacity).to eq(10)
+    expect(water_reservoir.current_water_volume).to eq(0)
+  end
+
 end
