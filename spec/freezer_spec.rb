1b5d28bfe79db0663533879d9cb05860d6487fd5
Seth Weiss <sweisss@gmail.com>
2023-03-15 20:30:26 -0700

    Add turn_on test in freezer_spec

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -8,4 +8,10 @@ describe 'A freezer' do
     expect(freezer.temperature).to eq(70)
   end
 
+  it 'can turn its power on' do
+    freezer = Freezer.new
+    freezer.turn_on
+    expect(freezer.power).to eq(:on)
+  end
+
 end
