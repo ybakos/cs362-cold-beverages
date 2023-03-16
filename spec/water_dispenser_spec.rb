3edc2462da693f66933f168d7799be474325dc09
Seth Weiss <sweisss@gmail.com>
2023-03-15 19:02:56 -0700

    Add initialize test to water_dispenser_spec

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -2,4 +2,9 @@ require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
 
+  it 'has a reservoir' do
+    water_dispenser = WaterDispenser.new(100)
+    expect(water_dispenser.reservoir).to eq(100)
+  end
+
 end
