24d057067613f9854860d1395ca2743b6e21c0f0
Seth Weiss <sweisss@gmail.com>
2023-03-15 20:25:30 -0700

    Add initialize test for freezer_spec

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -2,4 +2,10 @@ require_relative '../lib/freezer'
 
 describe 'A freezer' do
 
+  it 'has a capacity and temperature' do
+    freezer = Freezer.new
+    expect(freezer.capacity).to eq(100)
+    expect(freezer.temperature).to eq(70)
+  end
+
 end
