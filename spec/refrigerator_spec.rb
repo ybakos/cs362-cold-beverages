547eb01b07b673f312775639048bc99b6a8a986b
Seth Weiss <sweisss@gmail.com>
2023-03-15 21:46:20 -0700

    Add chill test to refrigerator_spec

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -12,11 +12,18 @@ describe 'A refrigerator' do
     water_reservoir
   ) }
 
+  let(:oat_milk) { Item.new('OatMilk', 22) }
+
   it 'initializes with 4 dependencies' do
     expect(refrigerator.chiller).to eq(chiller)
     expect(refrigerator.freezer).to eq(freezer)
     expect(refrigerator.water_dispenser).to eq(water_dispenser)
     expect(refrigerator.water_reservoir).to eq(water_reservoir)
+  end
+
+  it 'can add items to its chiller' do
+    refrigerator.chill(oat_milk)
+    expect(refrigerator.remaining_capacity).to eq(178)
   end
 
 end
