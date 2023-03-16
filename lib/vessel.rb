9d7a82daf9ef44fb999cb5c65dd2f5c352b97bf7
Seth Weiss <sweisss@gmail.com>
2023-03-15 18:28:13 -0700

    Add Vessel empty? method

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,14 @@
 class Vessel
-  attr_reader :volume, :name
+  attr_reader :volume, :name, :fill_level
 
   def initialize(name='FAKE', volume=100)
     @name = name
     @volume = volume
+    @fill_level = 0
+  end
+
+  def empty?
+    fill_level == 0
   end
 
 end
