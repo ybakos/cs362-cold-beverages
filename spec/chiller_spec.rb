1242ac66b3f4d45a6337389a81d43624ea5f2960
Seth Weiss <sweisss@gmail.com>
2023-03-15 20:57:09 -0700

    Add add items test in chiller_spec

--- spec/chiller_spec.rb
+++ spec/chiller_spec.rb
@@ -2,6 +2,8 @@ require_relative '../lib/chiller'
 
 describe 'A chiller' do
   let(:chiller) { Chiller.new }
+  let(:bagels) { Item.new('Bagels', 6) }
+  let(:oat_milk) { Item.new('OatMilk', 22) }
 
   it 'has a capacity and temperature' do
     expect(chiller.capacity).to eq(100)
@@ -16,6 +18,12 @@ describe 'A chiller' do
   it 'can turn power off' do
     chiller.turn_off
     expect(chiller.power).to eq(:off)
+  end
+
+  it 'can add items' do
+    chiller.add(bagels)
+    chiller.add(oat_milk)
+    expect(chiller.contents).to eq([bagels, oat_milk])
   end
 
 end
