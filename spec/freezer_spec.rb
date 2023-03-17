c6ea44ad98c645e5eeb7038ac13c01fae08132ec
Blake <bario54321@gmail.com>
2023-03-17 15:34:28 -0700

    Slight refactoring in vessel model, added can be turned on/off tests in freezer_spec

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -5,5 +5,20 @@ describe 'A freezer' do
   it 'has a capacity' do
     freezer = Freezer.new
     expect(freezer.capacity).to eq(100)
+    expect(freezer.temperature).to eq(70)
+    expect(freezer.power).to eq(:off)
+    expect(freezer.contents).to be_empty
+  end
+
+  it 'can be turned on' do
+    freezer = Freezer.new
+    freezer.turn_on
+    expect(freezer.power).to eq(:on)
+  end
+
+  it 'can be turned off' do
+    freezer = Freezer.new
+    freezer.turn_off
+    expect(freezer.power).to eq(:off)
   end
 end
