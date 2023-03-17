0da1c032a24cef1ba6cd22da02a14736805c5785
Blake <bario54321@gmail.com>
2023-03-17 14:40:08 -0700

    it can dispense water test passign in water_dispenser

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -2,4 +2,17 @@ require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
 
+  it 'exists' do
+    water_reservoir = WaterReservoir.new
+    water_dispenser = WaterDispenser.new(water_reservoir)
+  end
+
+  it 'can dispense water' do
+    water_reservoir = WaterReservoir.new(10, 10)
+    vessel = Vessel.new('FAKE', 5)
+    water_dispenser = WaterDispenser.new(water_reservoir)
+
+    water_dispenser.dispense(vessel)
+    expect(water_dispenser.reservoir.current_water_volume).to eq(5)
+  end
 end
