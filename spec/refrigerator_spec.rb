1004f20d6723dfea3dc50e29092b087e49b911e4
Blake <bario54321@gmail.com>
2023-03-17 16:06:59 -0700

    it exists, can chill/freeze an item tests passing in refrigerator_spec

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -2,4 +2,36 @@ require_relative '../lib/refrigerator'
 
 describe 'A refrigerator' do
 
+  it 'exists' do
+    chiller = Chiller.new
+    freezer = Freezer.new
+    water_reservoir = WaterReservoir.new
+    water_dispenser = WaterDispenser.new(water_reservoir)
+    refrigerator = Refrigerator.new(chiller, freezer, water_reservoir, water_dispenser)
+  end
+
+  it 'can chill an item' do
+    chiller = Chiller.new
+    freezer = Freezer.new
+    water_reservoir = WaterReservoir.new
+    water_dispenser = WaterDispenser.new(water_reservoir)
+    refrigerator = Refrigerator.new(chiller, freezer, water_reservoir, water_dispenser)
+    
+    item = Item.new('FAKE', 5)
+    refrigerator.chill(item)
+    expect(refrigerator.chiller.contents).to contain_exactly(item)
+  end
+
+  it 'can freeze an item' do
+    chiller = Chiller.new
+    freezer = Freezer.new
+    water_reservoir = WaterReservoir.new
+    water_dispenser = WaterDispenser.new(water_reservoir)
+    refrigerator = Refrigerator.new(chiller, freezer, water_reservoir, water_dispenser)
+    
+    item = Item.new('FAKE', 5)
+    refrigerator.freeze(item)
+    expect(refrigerator.freezer.contents).to contain_exactly(item)
+  end
+  
 end
