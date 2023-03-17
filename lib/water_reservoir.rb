afcb12ccfd0142d83e09f9b2793683b6893336f8
Blake <bario54321@gmail.com>
2023-03-17 16:22:17 -0700

    water reservoir drain function updated to not allow negative volume

--- lib/water_reservoir.rb
+++ lib/water_reservoir.rb
@@ -17,7 +17,11 @@ class WaterReservoir
   end
 
   def drain(volume)
-    self.current_water_volume -= volume
+    if current_water_volume >= volume
+      self.current_water_volume -= volume
+    else
+      self.current_water_volume -= current_water_volume
+    end
   end
 
 end
