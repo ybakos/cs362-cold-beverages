c6ed98221ddc9edaeab5f142690950445c7fb2f5
Blake Pugh <bario54321@gmail.com>
2023-03-17 12:35:41 -0700

    Added name, volume, fill, and empty? methods to vessel.rb

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,26 @@
 class Vessel
   attr_reader :volume
 
-  def initialize(name, volume)
+  def initialize(name='FAKE', volume=100)
     @name = name
     @volume = volume
+    @be_empty = true
+  end
+
+  def name
+    @name
+  end
+
+  def volume
+    @volume
+  end
+
+  def fill
+    @be_empty = false
+  end
+  
+  def empty?
+    @be_empty
   end
 
 end
