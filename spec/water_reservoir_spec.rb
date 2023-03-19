bd39a3dcd38202f0a7977332289ca592484c8ef6
Cooper Bailey <cjbailey02@gmail.com>
2023-03-19 12:25:03 -0700

    Completed fill/drain tests for water_reservoir

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -11,6 +11,10 @@ describe 'A water reservoir' do
     water_reservoir.fill
     expect(water_reservoir.volume).to eq(10)
   end
-  
+
+  it 'can be drained' do
+    water_reservoir.drain(water_reservoir.volume)
+    expect(water_reservoir.volume).to eq(0)
+  end
 
 end
