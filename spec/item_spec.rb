7d02975ac19df1fa84a7de5cd001817c7dba4ec0
Cooper Bailey <cjbailey02@gmail.com>
2023-03-19 09:17:52 -0700

    consolidated name and volume tests so there is only one test checking for both

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,13 +2,9 @@ require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
 
-  it 'has a name' do
+  it 'has a name and a volume' do
     pizza = Item.new('pizza', 5)
     expect(pizza.name).to eq('pizza')
-  end
-
-  it 'has a volume' do
-    pizza = Item.new('pizza', 5)
     expect(pizza.volume).to eq(5)
   end
 
