75a4faaf1e078cbd490af1eae1f93c50ef34f01a
Cooper Bailey <cjbailey02@gmail.com>
2023-03-19 09:16:46 -0700

    added tests for Item class to check whether new items have a name and volume associated with them

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,4 +2,14 @@ require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
 
+  it 'has a name' do
+    pizza = Item.new('pizza', 5)
+    expect(pizza.name).to eq('pizza')
+  end
+
+  it 'has a volume' do
+    pizza = Item.new('pizza', 5)
+    expect(pizza.volume).to eq(5)
+  end
+
 end
