70b86b9746b7e5a21fdf20c1a3586a6859f24d06
Cooper Bailey <cjbailey02@gmail.com>
2023-03-19 12:46:37 -0700

    Completed basic tests for freezer, including ability to add items to the freezer, calculate the remaining capacity of the freezer and set the temperature based on the level

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -17,4 +17,19 @@ describe 'A freezer' do
     expect(freezer).to be_empty
   end
 
+  it 'should be able to have items added to it' do
+    item = Item.new("pizza", 5)
+    freezer.add(item)
+    expect(freezer).to_not be_empty
+  end
+
+  it 'should be able to calculate the remaining capacity' do
+    expect(freezer.remaining_capacity).to eq(95)
+  end
+
+  it 'should be able to set the current temperature level' do
+    freezer.set_level(3)
+    expect(freezer.temperature).to eq(40)
+  end
+
 end
