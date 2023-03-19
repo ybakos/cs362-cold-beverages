3d4873a4c2a3ba2801aa17214686d222a458b3fe
Cooper Bailey <cjbailey02@gmail.com>
2023-03-19 12:19:48 -0700

    Checking that water_reservoir is empty on initialization

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -2,4 +2,9 @@ require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
 
+  water_reservoir = WaterReservoir.new
+  it 'should be empty when initialized' do
+    expect(water_reservoir).to be_empty
+  end
+
 end
