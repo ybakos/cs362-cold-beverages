737a7b8bfc7552d11549dc082bec0587321a485d
Cooper Bailey <cjbailey02@gmail.com>
2023-03-19 17:02:24 -0700

    Completed tests to check total_capacity function returns query for combined total of freezer and chiller capacity

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -1,5 +1,30 @@
 require_relative '../lib/refrigerator'
+require_relative '../lib/chiller'
+require_relative '../lib/freezer'
+require_relative '../lib/water_dispenser'
+require_relative '../lib/water_reservoir'
 
 describe 'A refrigerator' do
 
+  pizza = Item.new("pizza", 5)
+  dreamsicle = Item.new('Dreamsicle', 1)
+  chiller = Chiller.new
+  freezer = Freezer.new
+  water_reservoir = WaterReservoir.new
+  water_dispenser = WaterDispenser.new(water_reservoir)
+
+  refrigerator = Refrigerator.new(chiller, freezer, water_dispenser, water_reservoir)
+
+  # it 'adds item to chiller by chilling them' do
+  #   refrigerator.chill(pizza)
+  #   print(refrigerator.chiller)
+  #   expect(refrigerator.chiller).to include?(pizza)
+  # end
+
+  it 'total capacity includes both the freezers and the chillers capacity' do
+    expect(refrigerator.total_capacity).to eq(200)
+  end
+
+  # it '' do
+  # end
 end
