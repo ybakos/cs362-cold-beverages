38fcf29744b2006745c9b73ac51464699c29b1f6
ehjohnson154 <spiffy154@gmail.com>
2023-03-21 12:49:24 -0700

    finished vessel tests

--- spec/vessel_spec.rb
+++ spec/vessel_spec.rb
@@ -9,15 +9,13 @@ describe 'A vessel for holding liquid' do
   end
 
   it 'is initially empty' do
-    skip
     vessel = Vessel.new('FAKE',100)
-    expect(vessel).to be_empty
+    expect(vessel.fluid).to eq(0) #removed be empty
   end
 
   it 'is no longer empty when we fill it' do
-    skip
     vessel = Vessel.new('FAKE', 100)
     vessel.fill
-    expect(vessel).to_not be_empty
+    expect(vessel.fluid).to_not eq(0) #removed be empty
   end
 end
