8a2a07885b65154326a83618f0681155b44d2ea6
Ross Voerman <voermanr@oregonstate.edu>
2023-03-21 14:23:43 -0700

    moved variable creation into let functions in water_dispenser_spec.rb

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,10 +1,12 @@
 require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
+  let(:fake_water_reservoir) { double('WaterReservoir') }
+  let(:water_dispenser) { WaterDispenser.new(fake_water_reservoir) }
+  let(:fake_vessel) { double('Vessel') }
+
 
   it 'has a reservoir' do
-    fake_water_reservoir = double('WaterReservoir')
-    water_dispenser = WaterDispenser.new(fake_water_reservoir)
     expect(water_dispenser.reservoir).to be_truthy
   end
 
