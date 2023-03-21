76f06cd63d72a742ce237b92c9371fcc0499d912
ehjohnson154 <spiffy154@gmail.com>
2023-03-21 12:45:11 -0700

    got project up and running, completed first vessel test

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,26 @@
 class Vessel
   attr_reader :volume
 
-  def initialize(name, volume)
+  def initialize(name, volume, fluid = 0)
     @name = name
     @volume = volume
+    @fluid = fluid
+  end
+
+  def name
+    @name
+  end
+
+  def volume
+    @volume
+  end
+
+  def fluid
+    @fluid
+  end
+
+  def fill
+    @fluid = volume
   end
 
 end
--- spec/vessel_spec.rb
+++ spec/vessel_spec.rb
@@ -3,14 +3,14 @@ require_relative '../lib/vessel'
 describe 'A vessel for holding liquid' do
 
   it 'has a name and volume' do
-    vessel = Vessel.new
-    expect(vessel.name).to eq('FAKE')
+    vessel = Vessel.new('FAKE', 100)
+    expect(vessel.name ).to eq('FAKE')
     expect(vessel.volume).to eq(100)
   end
 
   it 'is initially empty' do
     skip
-    vessel = Vessel.new('FAKE', 100)
+    vessel = Vessel.new('FAKE',100)
     expect(vessel).to be_empty
   end
 
