a5fbf195d97b4e7b364dec13b58f72239a6847f0
Ross Voerman <voermanr@oregonstate.edu>
2023-03-21 15:13:32 -0700

    characterization test for remaining_capacity() in freezer_spec.rb

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -3,6 +3,8 @@ require_relative '../lib/freezer'
 describe 'A freezer' do
   let(:freezer) { FreezerTest.new }
   let(:item) { double('Item') }
+  let(:small_item) { double('Item') }
+  let(:big_item) { double('Item') }
 
   it 'has a capacity and temperature' do
     expect(freezer.capacity).to be_truthy
@@ -31,6 +33,17 @@ describe 'A freezer' do
     freezer.add item
 
     expect(freezer.contents).to include item
+  end
+
+  it 'has a sensible remaining_capacity' do
+    freezer.add small_item
+    freezer.add big_item
+
+    allow(small_item).to receive(:volume).and_return(1)
+    allow(big_item).to receive(:volume).and_return(20)
+    a_sensible_remaining_capacity = freezer.capacity - small_item.volume - big_item.volume
+
+    expect(freezer.remaining_capacity).to be a_sensible_remaining_capacity
   end
 
 end
