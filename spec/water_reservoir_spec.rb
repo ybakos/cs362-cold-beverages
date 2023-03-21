2cb6df4370146061e00b6e7483bed5b0ae144732
Ross Voerman <voermanr@oregonstate.edu>
2023-03-21 14:30:21 -0700

    characterization test of initialize() in water_reservoir_spec.rb

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/water_reservoir'
 
 describe 'A water reservoir' do
+  let(:water_reservoir) { WaterReservoir.new }
 
+  it 'has a capacity and current water volume' do
+    expect(water_reservoir.capacity).to be_truthy
+    expect(water_reservoir.current_water_volume).to be_truthy
+  end
 end
