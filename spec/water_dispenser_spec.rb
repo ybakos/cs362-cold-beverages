cfccaab26961b4fb30b4d8456442cfbfd9ef7525
Ross Voerman <voermanr@oregonstate.edu>
2023-03-21 14:05:23 -0700

    implemented characterization test for initialize() in water_dispenser_spec.rb

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -3,6 +3,7 @@ require_relative '../lib/water_dispenser'
 describe 'A water dispenser' do
 
   it 'has a reservoir' do
+    fake_water_reservoir = double('WaterReservoir')
     water_dispenser = WaterDispenser.new(fake_water_reservoir)
     expect(water_dispenser.reservoir).to be_truthy
   end
