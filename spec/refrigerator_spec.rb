e85080bdebd57d6f24c917d3b427cfca5ee272d7
Ross Voerman <voermanr@oregonstate.edu>
2023-03-21 15:33:44 -0700

    characterization test for initialization() in refrigerator_spec.rb

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -1,6 +1,20 @@
 require_relative '../lib/refrigerator'
 
 describe 'A refrigerator' do
+  let(:chiller) { double 'Chiller' }
+  let(:freezer) { double 'Freezer' }
+  let(:water_dispenser) { double 'WaterDispenser' }
+  let(:water_reservoir) { double 'WaterReservoir' }
+  let(:refrigerator) { RefrigeratorTest.new(chiller, freezer, water_dispenser, water_reservoir)}
+
+  it 'has a chiller, freezer, control_panel, water dispenser, water reservoir, and power status' do
+    expect(refrigerator.chiller).to be chiller
+    expect(refrigerator.freezer).to be freezer
+    expect(refrigerator.control_panel).to be_truthy
+    expect(refrigerator.water_dispenser).to be water_dispenser
+    expect(refrigerator.water_reservoir).to be water_reservoir
+    expect(refrigerator.power).to be_truthy
+  end
 
 end
 
