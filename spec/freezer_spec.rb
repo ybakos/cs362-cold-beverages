a80e50548ab30686e568870e16b6cf9f8af9bd9d
Ross Voerman <voermanr@oregonstate.edu>
2023-03-21 15:00:30 -0700

    better characterization test of initialize() in freezer_spec.rb

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -1,11 +1,13 @@
 require_relative '../lib/freezer'
 
 describe 'A freezer' do
-  let(:freezer) { Freezer.new }
+  let(:freezer) { FreezerTest.new }
 
   it 'has a capacity and temperature' do
     expect(freezer.capacity).to be_truthy
     expect(freezer.temperature).to be_truthy
+    expect(freezer.power).to be_truthy
+    expect(freezer.contents).to be_truthy
   end
 
 end
