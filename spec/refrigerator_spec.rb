01390649828718c6b2f5d435ef58ceea9bd5cb71
Ross Voerman <voermanr@oregonstate.edu>
2023-03-21 15:33:20 -0700

    added RefrigeratorTest to refrigerator_spec.rb

--- spec/refrigerator_spec.rb
+++ spec/refrigerator_spec.rb
@@ -3,3 +3,16 @@ require_relative '../lib/refrigerator'
 describe 'A refrigerator' do
 
 end
+
+class RefrigeratorTest < Refrigerator
+  attr_reader :power
+
+  def initialize(chiller, freezer, water_dispenser, water_reservoir)
+    super
+  end
+
+  # @todo remove when ControlPanels are implemented
+  def control_panel
+    true
+  end
+end
